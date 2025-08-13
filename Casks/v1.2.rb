53470ff5ec9b006f20b52dfada5f81e020722398
Terraform <terraform@redteam.local>
2025-08-13 13:35:49 -0400

    Add ChatGPT v1.2 cask

--- Casks/v1.2.rb
+++ Casks/v1.2.rb
@@ -1,6 +1,6 @@
 cask "v1.2" do
   version "1.2.025.210"
-  sha256 "388b851f3bc06f3fb622cf55fd17f278691f6714c50ad67a546330563ccb09ae"
+  sha256 "8697aeddb45f064c6bb21589229e1c27d9fa7e9cb2380bbcaf4741a516a6d6be"
   url "https://d3vi63ssapyaka.cloudfront.net/chatgpt/1.2025.210/setup.zip"
   name "ChatGPT Desktop v1.2"
   desc "ChatGPT Desktop v1.2"
